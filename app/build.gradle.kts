4c3f92faf0de70cc9e31e2a1083f98afe2eea339
Alexander Vinogradow <alukard981@gmail.com>
2024-12-19 19:36:39 +0300

    in build.gradle.kts: added dependencies for 1. authentification 2. hikariCp, postgreSql (for external DB connection) 3. Validation (for checking the entites for CRUD) 4. MapStruct and jackson databind nullable (for mapping objects) 5. ben manes for getting latest versions of dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 	id("io.freefair.lombok") version "8.6"
 	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("com.github.ben-manes.versions") version "0.49.0"
 }
 
 group = "hexlet.code"
@@ -29,13 +30,20 @@ application {
 }
 
 dependencies {
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
+	implementation("com.zaxxer:HikariCP:5.0.1")
+	implementation("org.postgresql:postgresql:42.7.2")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testImplementation(platform("org.junit:junit-bom:5.10.0"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
