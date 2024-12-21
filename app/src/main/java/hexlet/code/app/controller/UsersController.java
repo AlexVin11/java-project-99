b4768c62a0348088809471d0844d116a049ac6cf
Alexander Vinogradow <alukard981@gmail.com>
2024-12-21 17:52:29 +0300

    UsersController now uses a UserService

--- app/src/main/java/hexlet/code/app/controller/UsersController.java
+++ app/src/main/java/hexlet/code/app/controller/UsersController.java
@@ -1,30 +1,57 @@
 package hexlet.code.app.controller;
 
-import hexlet.code.app.model.User;
-import hexlet.code.app.repository.UserRepository;
+import hexlet.code.app.dto.UserCreateDTO;
+import hexlet.code.app.dto.UserDTO;
+import hexlet.code.app.dto.UserUpdateDTO;
+import hexlet.code.app.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api")
 public class UsersController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
-    @GetMapping()
-    public List<User> index() {
-        return userRepository.findAll();
+    @GetMapping("/users")
+    public List<UserDTO> index() {
+        return userService.getAll();
     }
 
-    @GetMapping("/{id}")
-    public Optional<User> show(@PathVariable Long id) {
-        return userRepository.findById(id);
+    @GetMapping("/users/{id}")
+    public UserDTO show(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO create(@Valid @RequestBody UserCreateDTO userData) {
+        var dto = userService.create(userData);
+        return dto;
+    }
+
+    @PutMapping("/users")
+    public UserDTO update(@Valid @RequestBody UserUpdateDTO userData, Long id) {
+        var dto = userService.update(userData, id);
+        return dto;
+    }
+
+    @DeleteMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(Long id) {
+        userService.delete(id);
     }
 }
