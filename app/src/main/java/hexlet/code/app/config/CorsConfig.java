636609b275a3e512ecc987843a388a24068cb82c
Alexander Vinogradow <alukard981@gmail.com>
2025-01-20 20:41:15 +0300

    CorsConfig.java class created to configure the settings (i will remove it)

---
+++ app/src/main/java/hexlet/code/app/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package hexlet.code.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configuration.addAllowedOrigin("http://localhost:8080");
+        configuration.addAllowedOrigin("https://taskmanager-v3pj.onrender.com");
+        configuration.addAllowedMethod(CorsConfiguration.ALL);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
