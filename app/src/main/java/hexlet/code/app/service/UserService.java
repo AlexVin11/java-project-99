6c912775566d218a2486fa77385422637ca0fa0c
Alexander Vinogradow <alukard981@gmail.com>
2025-01-22 20:21:01 +0300

    Now UsersController.java and UserService.java edited to work with the pageable interface. It allows to operate with request params like how much users to show on page, how to odering it (asc, desc)

--- app/src/main/java/hexlet/code/app/service/UserService.java
+++ app/src/main/java/hexlet/code/app/service/UserService.java
@@ -10,10 +10,11 @@ import hexlet.code.app.model.User;
 import hexlet.code.app.repository.UserRepository;
 import hexlet.code.app.util.UserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class UserService {
@@ -30,12 +31,8 @@ public class UserService {
     @Autowired
     private UserUtils userUtils;
 
-    public List<UserDTO> getAll() {
-        var users = userRepository.findAll();
-        var result = users.stream()
-                .map(userMapper::map)
-                .toList();
-        return result;
+    public Page<UserDTO> getAll(Pageable pageable) {
+        return userRepository.findAll(pageable).map(userMapper::map);
     }
 
     public UserDTO getById(Long id) {
