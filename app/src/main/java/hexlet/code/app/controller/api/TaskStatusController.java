a791577a2cc4ff1785f574b50e1e4186857807e6
Alexander Vinogradow <alukard981@gmail.com>
2025-01-24 13:33:58 +0300

    Example of good practise. No Autowired annotation, class fields is private final, class has AllArgsConstructor. So in future use you cant make mistake when class instance will be created in tests for example.

---
+++ app/src/main/java/hexlet/code/app/controller/api/TaskStatusController.java
@@ -0,0 +1,61 @@
+package hexlet.code.app.controller.api;
+
+import hexlet.code.app.dto.TaskStatusDTO.TaskStatusCreateDTO;
+import hexlet.code.app.dto.TaskStatusDTO.TaskStatusDTO;
+import hexlet.code.app.dto.TaskStatusDTO.TaskStatusUpdateDTO;
+import hexlet.code.app.service.TaskStatusService;
+import hexlet.code.app.util.UserUtils;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/task_statuses")
+public final class TaskStatusController {
+    private final UserUtils userUtils;
+    private final TaskStatusService taskStatusService;
+
+    @GetMapping()
+    public ResponseEntity<List<TaskStatusDTO>> index() {
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(taskStatusService.getAll()))
+                .body(taskStatusService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public TaskStatusDTO show(@PathVariable Long id) {
+        return taskStatusService.getById(id);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskStatusDTO create(@Valid @RequestBody TaskStatusCreateDTO taskStatusData) {
+        var dto = taskStatusService.create(taskStatusData);
+        return dto;
+    }
+
+    @PutMapping("/{id}")
+    public TaskStatusDTO update(@Valid @RequestBody TaskStatusUpdateDTO taskStatusData, @PathVariable Long id) {
+        var dto = taskStatusService.update(taskStatusData, id);
+        return dto;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        taskStatusService.delete(id);
+    }
+}
