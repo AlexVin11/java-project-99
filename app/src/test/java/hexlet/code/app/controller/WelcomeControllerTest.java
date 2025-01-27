8c2f0b75957608becaa87fa6c70263b7ae1f9d2a
Alexander Vinogradow <alukard981@gmail.com>
2025-01-27 20:44:18 +0300

    WelcomeControllerTest created

---
+++ app/src/test/java/hexlet/code/app/controller/WelcomeControllerTest.java
@@ -0,0 +1,27 @@
+package hexlet.code.app.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class WelcomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testWelcome() throws Exception {
+        var result = mockMvc.perform(get("/welcome"))
+                .andExpect(status().isOk())
+                .andReturn();
+        var body = result.getResponse().getContentAsString();
+        assertThat(body.contains("Welcome"));
+    }
+}
