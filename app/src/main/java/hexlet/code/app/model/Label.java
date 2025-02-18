bf242e5f35219d7159cc9ef21f0c3153dc24d1c7
Alexander Vinogradow <alukard981@gmail.com>
2025-02-18 22:40:03 +0300

    Label.java created

---
+++ app/src/main/java/hexlet/code/app/model/Label.java
@@ -0,0 +1,52 @@
+package hexlet.code.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "labels")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Label implements BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    @NotBlank
+    @EqualsAndHashCode.Include
+    @Size(min = 3, max = 1000)
+    private String name;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+
+    public Label(String name) {
+        this.name = name;
+    }
+}
