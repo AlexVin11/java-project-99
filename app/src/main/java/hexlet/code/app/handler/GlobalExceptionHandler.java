635ecf0b1f449f1ac99d72d93440138ae4305a8a
Alexander Vinogradow <alukard981@gmail.com>
2025-02-18 22:39:43 +0300

    500 server error added and commented because test User with incorrect token is failed (expected 403 but with this handler 500)

--- app/src/main/java/hexlet/code/app/handler/GlobalExceptionHandler.java
+++ app/src/main/java/hexlet/code/app/handler/GlobalExceptionHandler.java
@@ -19,4 +19,10 @@ public final class GlobalExceptionHandler extends ResponseEntityExceptionHandler
     public ResponseEntity<String> handleUnprocessableResourceException(DataIntegrityViolationException ex) {
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ex.getMessage());
     }
+    //закомментировал потому что падает ошибка
+    // в тесте удаление пользователя с невереным токеном (с этим эендлером падает 500)
+    /*@ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllOtherExceptions(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }*/
 }
