1ea8fa97d80c198b1e9bb6d29073f41fcbb47ed8
Alexander Vinogradow <alukard981@gmail.com>
2025-02-18 22:52:41 +0300

    minor

--- app/src/main/java/hexlet/code/app/repository/TaskRepository.java
+++ app/src/main/java/hexlet/code/app/repository/TaskRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByAssigneeId(Long id);
